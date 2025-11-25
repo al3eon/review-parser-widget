29c54d73e899ec514cc58ed1f416ad0ebda64a36
Lev Azaretov <azaretov123@gmail.com>
2025-11-26 00:08:36 +0300

    chore: add Docker and CI pipeline

--- scraper/browser.py
+++ scraper/browser.py
@@ -1,23 +1,36 @@
+import os
+
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
 
 from scraper.constants import USER_AGENT
 
+load_dotenv()
+
 
 def init_driver():
     options = Options()
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_argument(f'--user-agent={USER_AGENT}')
+    selenium_url = os.getenv('SELENIUM_URL')
 
-    driver = webdriver.Chrome(options=options)
+    if selenium_url:
+        driver = webdriver.Remote(
+            command_executor=selenium_url,
+            options=options
+        )
+    else:
+        driver = webdriver.Chrome(options=options)
+        stealth(driver,
+                languages=['ru-RU', 'ru'],
+                vendor='Google Inc.',
+                platform='Win32',
+                webgl_vendor='Intel Inc.',
+                renderer='Intel Iris OpenGL Engine',
+                fix_hairline=True,
+                )
+
     driver.set_window_size(1920, 1080)
-    stealth(driver,
-            languages=['ru-RU', 'ru'],
-            vendor='Google Inc.',
-            platform='Win32',
-            webgl_vendor='Intel Inc.',
-            renderer='Intel Iris OpenGL Engine',
-            fix_hairline=True,
-            )
     return driver
